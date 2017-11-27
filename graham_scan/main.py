c85898f4f43ce1cfbd484ee04794f40a1e586f14
Brian Faure <bfaure23@gmail.com>
2017-11-27 02:43:00 -0500

    optimized, refactored

--- graham_scan/main.py
+++ graham_scan/main.py
@@ -3,6 +3,7 @@ from matplotlib import pyplot as plt # for plotting
 import numpy as np # for plotting
 from random import randint # for creating data points
 from math import atan2 # for computing polar angle
+from time import time # for timing benchmark
 
 # Creates a scatter plot, input is a list of (x,y) coordinates.
 # The second input 'convex_hull' is another list of (x,y) coordinates
@@ -10,21 +11,18 @@ from math import atan2 # for computing polar angle
 # if not None, the elements of this list will be used to draw the outer
 # boundary (the convex hull surrounding the data points).
 def scatter_plot(coords,convex_hull=None):
-	xs=[pt[0] for pt in coords] # x coordinates
-	ys=[pt[1] for pt in coords] # y coordinates
-
+	xs,ys=zip(*coords) # unzip into x and y coord lists
 	xs,ys=np.array(xs),np.array(ys) # convert to numpy arrays
 	plt.scatter(xs,ys) # plot the data points
 
 	if convex_hull!=None:
-		# plot the convex hull boundary
-		for i in range(1,len(convex_hull)):
+		# plot the convex hull boundary, extra iteration at
+		# the end so that the bounding line wraps around
+		for i in range(1,len(convex_hull)+1):
+			if i==len(convex_hull): i=0 # wrap
 			c0=convex_hull[i-1]
 			c1=convex_hull[i]
 			plt.plot((c0[0],c1[0]),(c0[1],c1[1]),'r')
-		first=convex_hull[0]
-		last=convex_hull[-1]
-		plt.plot((last[0],first[0]),(last[1],first[1]),'r')
 	plt.show() # show the plot
 
 
@@ -34,19 +32,29 @@ def scatter_plot(coords,convex_hull=None):
 def create_points(num_points,min=0,max=50):
 	return [[randint(min,max),randint(min,max)] for _ in range(num_points)]
 
+
 # Returns the polar angle (radians) from p0 to p1.
-# p0 is assumed to be the point with the lowest y coordinate.
-def polar_angle(p0,p1):
+# If p1 is None, defaults to replacing it with the
+# global variable 'anchor', normally set in the 
+# 'graham_scan' function.
+def polar_angle(p0,p1=None):
+	if p1 is None: p1=anchor
 	y_span=p1[1]-p0[1]
 	x_span=p1[0]-p0[0]
 	return atan2(y_span,x_span)
 
+
 # Returns the euclidean distance from p0 to p1,
-# square root is not applied for sake of speed
-def distance(p0,p1):
+# square root is not applied for sake of speed.
+# If p1 is None, defaults to replacing it with the
+# global variable 'anchor', normally set in the 
+# 'graham_scan' function.
+def distance(p0,p1=None):
+	if p1 is None: p1=anchor
 	y_span=p1[1]-p0[1]
 	x_span=p1[0]-p0[0]
 	return y_span**2+x_span**2
+
 
 # Sorts in order of increasing polar angle from 'anchor' point.
 # 'anchor' variable is assumed to be global, set from within 'graham_scan'.
@@ -54,27 +62,15 @@ def distance(p0,p1):
 # ensure increasing distance from the 'anchor' point.
 def quicksort(a):
 	if len(a)<=1: return a 
-	smaller,equal,larger=[],[],[]
-	piv=a[randint(0,len(a)-1)] # select random pivot
-	piv_ang=polar_angle(anchor,piv) # calculate pivot angle
+	smaller,equal,larger=[],[],[] 
+	piv_ang=polar_angle(a[randint(0,len(a)-1)]) # select random pivot
 	for pt in a:
-		pt_ang=polar_angle(anchor,pt) # calculate current pt angle
-		if pt_ang<piv_ang: 		smaller.append(pt)
-		elif pt_ang==piv_ang:
-			# if polar angle is the same, sort by increasing distance
-			equal.append(pt)
-			temp=[]
-			while len(equal)>0:
-				min_dist,min_idx=None,None
-				for i,e in enumerate(equal):
-					if min_dist==None or distance(e,anchor)<min_dist:
-						min_dist=distance(e,anchor)
-						min_idx=i 
-				temp.append(equal[min_idx])
-				del equal[min_idx]
-			equal=temp
-		else:					larger.append(pt)
-	return quicksort(smaller)+equal+quicksort(larger)
+		pt_ang=polar_angle(pt) # calculate current point angle
+		if   pt_ang<piv_ang:  smaller.append(pt)
+		elif pt_ang==piv_ang: equal.append(pt)
+		else:                 larger.append(pt)
+	return quicksort(smaller)+sorted(equal,key=distance)+quicksort(larger)
+
 
 # Returns the determinant of the 3x3 matrix...
 # 	[p1(x) p1(y) 1]
@@ -85,6 +81,7 @@ def quicksort(a):
 # If =0 then collinear
 def determinant(p1,p2,p3):
 	return (p2[0]-p1[0])*(p3[1]-p1[1])-(p2[1]-p1[1])*(p3[0]-p1[0])
+
 
 # Returns the vertices comprising the boundaries of
 # convex hull containing all points in the input set. 
@@ -98,17 +95,21 @@ def graham_scan(points,show_progress=False):
 	# along with its index in the 'points' list. If
 	# there are multiple points with the same y value,
 	# choose the one with smallest x.
-	min_y,min_idx=None,None
+	min_idx=None
 	for i,(x,y) in enumerate(points):
-		if min_y==None or y<min_y:
-			min_y,min_idx=y,i 
-		if y==min_y and x<points[min_idx][0]:
+		if min_idx==None or y<points[min_idx][1]:
 			min_idx=i
-	anchor=points[min_idx] # pt with min y value
+		if y==points[min_idx][1] and x<points[min_idx][0]:
+			min_idx=i 
 
-	sorted_pts=points[:] # make copy of input points
-	del sorted_pts[min_idx] # remove anchor point
-	sorted_pts=quicksort(sorted_pts) # sort the points by polar angle
+	# set the global variable 'anchor', used by the
+	# 'polar_angle' and 'distance' functions
+	anchor=points[min_idx] # point with min y value
+
+	# sort the points by polar angle then delete 
+	# the anchor from the sorted list
+	sorted_pts=quicksort(points) 
+	del sorted_pts[sorted_pts.index(anchor)] 	
 
 	# anchor and point with smallest polar angle will always be on hull
 	hull=[anchor,sorted_pts[0]] 
@@ -125,8 +126,32 @@ def graham_scan(points,show_progress=False):
 		if show_progress: scatter_plot(points,hull)
 	return hull 
 
+
+####################################
+
+
+# For each size in the 'sizes' list, compute the average
+# time to find the Convex Hull for a dataset of that size,
+# the range used for max and min for the create_points function
+# is always 10 times the highest value in the 'sizes' list.
+def benchmark(sizes=[10,100,1000,10000,100000]):
+	for s in sizes:
+		tot=0.0
+		for _ in range(3):
+			pts=create_points(s,0,max(sizes)*10)
+			t0=time()
+			hull=graham_scan(pts,False)
+			tot+=(time()-t0)
+		print "size %d time: %0.5f"%(s,tot/5.0)
+
+'''
+benchmark()
+#'''
+#'''
 pts=create_points(10,0,500)
 print "points:",pts
 hull=graham_scan(pts,False)
 print "hull:",hull
 scatter_plot(pts,hull)
+print "hull:",hull
+#'''
