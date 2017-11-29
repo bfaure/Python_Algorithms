d8b77212b9edae3c5567f4f63b6012e6ce46e788
Brian Faure <bfaure23@gmail.com>
2017-11-28 20:27:04 -0500

    improvements

--- graham_scan/main.py
+++ graham_scan/main.py
@@ -1,9 +1,14 @@
 
 from matplotlib import pyplot as plt # for plotting
-import numpy as np # for plotting
-from random import randint # for creating data points
+from random import randint # for sorting and creating data pts
 from math import atan2 # for computing polar angle
-from time import time # for timing benchmark
+
+# Returns a list of (x,y) coordinates of length 'num_points',
+# each x and y coordinate is chosen randomly from the range 
+# 'min' up to 'max'.
+def create_points(ct,min=0,max=50):
+	return [[randint(min,max),randint(min,max)] \
+			for _ in range(ct)]
 
 # Creates a scatter plot, input is a list of (x,y) coordinates.
 # The second input 'convex_hull' is another list of (x,y) coordinates
@@ -12,7 +17,6 @@ from time import time # for timing benchmark
 # boundary (the convex hull surrounding the data points).
 def scatter_plot(coords,convex_hull=None):
 	xs,ys=zip(*coords) # unzip into x and y coord lists
-	xs,ys=np.array(xs),np.array(ys) # convert to numpy arrays
 	plt.scatter(xs,ys) # plot the data points
 
 	if convex_hull!=None:
@@ -23,14 +27,7 @@ def scatter_plot(coords,convex_hull=None):
 			c0=convex_hull[i-1]
 			c1=convex_hull[i]
 			plt.plot((c0[0],c1[0]),(c0[1],c1[1]),'r')
-	plt.show() # show the plot
-
-
-# Returns a list of (x,y) coordinates of length 'num_points',
-# each x and y coordinate is chosen randomly from the range 
-# 'min' up to 'max'.
-def create_points(num_points,min=0,max=50):
-	return [[randint(min,max),randint(min,max)] for _ in range(num_points)]
+	plt.show()
 
 
 # Returns the polar angle (radians) from p0 to p1.
@@ -38,9 +35,9 @@ def create_points(num_points,min=0,max=50):
 # global variable 'anchor', normally set in the 
 # 'graham_scan' function.
 def polar_angle(p0,p1=None):
-	if p1 is None: p1=anchor
-	y_span=p1[1]-p0[1]
-	x_span=p1[0]-p0[0]
+	if p1==None: p1=anchor
+	y_span=p0[1]-p1[1]
+	x_span=p0[0]-p1[0]
 	return atan2(y_span,x_span)
 
 
@@ -50,26 +47,10 @@ def polar_angle(p0,p1=None):
 # global variable 'anchor', normally set in the 
 # 'graham_scan' function.
 def distance(p0,p1=None):
-	if p1 is None: p1=anchor
-	y_span=p1[1]-p0[1]
-	x_span=p1[0]-p0[0]
-	return y_span**2+x_span**2
-
-
-# Sorts in order of increasing polar angle from 'anchor' point.
-# 'anchor' variable is assumed to be global, set from within 'graham_scan'.
-# For any values with equal polar angles, a second sort is applied to
-# ensure increasing distance from the 'anchor' point.
-def quicksort(a):
-	if len(a)<=1: return a 
-	smaller,equal,larger=[],[],[] 
-	piv_ang=polar_angle(a[randint(0,len(a)-1)]) # select random pivot
-	for pt in a:
-		pt_ang=polar_angle(pt) # calculate current point angle
-		if   pt_ang<piv_ang:  smaller.append(pt)
-		elif pt_ang==piv_ang: equal.append(pt)
-		else:                 larger.append(pt)
-	return quicksort(smaller)+sorted(equal,key=distance)+quicksort(larger)
+	if p1==None: p1=anchor
+	y_span=p0[1]-p1[1]
+	x_span=p0[0]-p1[0]
+	return y_span**2 + x_span**2
 
 
 # Returns the determinant of the 3x3 matrix...
@@ -80,7 +61,26 @@ def quicksort(a):
 # If <0 then clockwise
 # If =0 then collinear
 def det(p1,p2,p3):
-	return (p2[0]-p1[0])*(p3[1]-p1[1])-(p2[1]-p1[1])*(p3[0]-p1[0])
+	return   (p2[0]-p1[0])*(p3[1]-p1[1]) \
+			-(p2[1]-p1[1])*(p3[0]-p1[0])
+
+
+# Sorts in order of increasing polar angle from 'anchor' point.
+# 'anchor' variable is assumed to be global, set from within 'graham_scan'.
+# For any values with equal polar angles, a second sort is applied to
+# ensure increasing distance from the 'anchor' point.
+def quicksort(a):
+	if len(a)<=1: return a
+	smaller,equal,larger=[],[],[]
+	piv_ang=polar_angle(a[randint(0,len(a)-1)]) # select random pivot
+	for pt in a:
+		pt_ang=polar_angle(pt) # calculate current point angle
+		if   pt_ang<piv_ang:  smaller.append(pt)
+		elif pt_ang==piv_ang: equal.append(pt)
+		else: 				  larger.append(pt)
+	return   quicksort(smaller) \
+			+sorted(equal,key=distance) \
+			+quicksort(larger)
 
 
 # Returns the vertices comprising the boundaries of
@@ -88,7 +88,7 @@ def det(p1,p2,p3):
 # The input 'points' is a list of (x,y) coordinates.
 # If 'show_progress' is set to True, the progress in 
 # constructing the hull will be plotted on each iteration.
-def graham_scan(points,show_progress=False):	
+def graham_scan(points,show_progress=False):
 	global anchor # to be set, (x,y) with smallest y value
 
 	# Find the (x,y) point with the lowest y value,
@@ -100,35 +100,26 @@ def graham_scan(points,show_progress=False):
 		if min_idx==None or y<points[min_idx][1]:
 			min_idx=i
 		if y==points[min_idx][1] and x<points[min_idx][0]:
-			min_idx=i 
+			min_idx=i
 
 	# set the global variable 'anchor', used by the
 	# 'polar_angle' and 'distance' functions
-	anchor=points[min_idx] # point with min y value
+	anchor=points[min_idx]
 
 	# sort the points by polar angle then delete 
 	# the anchor from the sorted list
-	sorted_pts=quicksort(points) 
-	del sorted_pts[sorted_pts.index(anchor)] 	
+	sorted_pts=quicksort(points)
+	del sorted_pts[sorted_pts.index(anchor)]
 
 	# anchor and point with smallest polar angle will always be on hull
-	hull=[anchor,sorted_pts[0]] 
-
-	# iterate over points in increasing polar angle
+	hull=[anchor,sorted_pts[0]]
 	for s in sorted_pts[1:]:
-		if det(hull[-2],hull[-1],s)>0: # counter clockwise
-			hull.append(s)
-		else: # clockwise or linear
-			while det(hull[-2],hull[-1],s)<=0:
-				del hull[-1]
-				if len(hull)<2: break
-			hull.append(s)
+		while det(hull[-2],hull[-1],s)<=0:
+			del hull[-1] # backtrack
+			if len(hull)<2: break
+		hull.append(s)
 		if show_progress: scatter_plot(points,hull)
-	return hull 
-
-
-####################################
-
+	return hull
 
 # For each size in the 'sizes' list, compute the average
 # time to find the Convex Hull for a dataset of that size,
@@ -144,14 +135,9 @@ def benchmark(sizes=[10,100,1000,10000,100000]):
 			tot+=(time()-t0)
 		print "size %d time: %0.5f"%(s,tot/3.0)
 
-#'''
-benchmark()
-#'''
-'''
-pts=create_points(10,0,500)
-print "points:",pts
-hull=graham_scan(pts,False)
-print "hull:",hull
+
+pts=create_points(10)
+print "Points:",pts
+hull=graham_scan(pts,True)
+print "Hull:",hull
 scatter_plot(pts,hull)
-print "hull:",hull
-#'''
